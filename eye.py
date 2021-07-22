cd1fad400619d29fb73369b83145960486ba70cb
riaz34 <52790238+riaz34@users.noreply.github.com>
2021-07-22 08:13:14 +0600

    Update eye.py

--- eye.py
+++ eye.py
@@ -9,6 +9,10 @@ port = input("Enter the port:")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 if sock.connect_ex((ip,port)):
+       print("#####################################################")
        print "Port",port,"is closed :("
+       print("#####################################################")
 else:
+         print("#####################################################") 
          print "Port",port,"is OPEN :)"
+         print("#####################################################")
